f24f4fb155c6de00c4480e4c54555ded37aea576
KirillKozlovsky <kozlovskykirill@gmail.com>
2021-08-30 14:12:29 +0300

    Added many shit

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,14 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         int x = 1;
+        x = 1;
+        x = 1;
+        x = 1;
+        x = 1;
+        x = 1;
+        x = 1;
+        x = 1;
+        x = 1;
+        x = 1;
     }
 }
