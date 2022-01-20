f11a983854f5959334c0c02bef0950c1badd1898
Kirill Kozlovsky <kozlovskykirill@gmail.com>
2022-01-20 23:05:41 +0200

    Exercise 35

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
+import com.kirillkozlovsky.section6.Circle;
+import com.kirillkozlovsky.section6.Cylinder;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Circle circle = new Circle(3.75);
+        System.out.println("circle.radius= " + circle.getRadius());
+        System.out.println("circle.area= " + circle.getArea());
+        Cylinder cylinder = new Cylinder(5.55, 7.25);
+        System.out.println("cylinder.radius= " + cylinder.getRadius());
+        System.out.println("cylinder.height= " + cylinder.getHeight());
+        System.out.println("cylinder.area= " + cylinder.getArea());
+        System.out.println("cylinder.volume= " + cylinder.getVolume());
     }
 }
